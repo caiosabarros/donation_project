1fcd9975a612bd09f5e6b60d5ae11719f99a375e
Caio Sa <caiohenriquesa1@gmail.com>
2022-01-20 11:23:00 -0300

    Comments for improvements

--- Donation.sol
+++ Donation.sol
@@ -26,7 +26,7 @@ pragma solidity ^0.8.7;
 contract Donation {
 
     
-
+    //Project Structure
     struct Project {
         address payable representant;
         uint256 fundingGoal;
@@ -37,23 +37,33 @@ contract Donation {
 
     }
 
+    //Variable to Enumerate Projects
     uint256 numProjects;
+    //projetcs mappings that maps an ID to a Project Structure
     mapping(uint256 => Project) public projects;
 
     function addProject (uint256 _goal, address payable _representant) public payable returns (uint256 project_Id) {
+        //Each time a project is created, project_Id is receive numProjects and is incremented 
         project_Id = numProjects++;
+        //variable p to refer to a Project Strucutre of a Specific Id from the projects mapping
         Project storage p = projects[project_Id];
+        //Define the goal and the representant of the Project of ID equals to project_Id
         p.representant = _representant;
         p.fundingGoal =  _goal;
     }
 
     function getBalance(uint256 _projectId) public view returns (uint256) {
+        //variable p to refer to a Project Strucutre of a Specific Id from the projects mapping
         Project storage p = projects[_projectId];
         return p.balance;
     }
 
     function donate(uint256 _projectId) public payable returns (bool){
+        //variable p to refer to a Project Strucutre of a Specific Id from the projects mapping
         Project storage p = projects[_projectId];
+        //I think I can better this part. If the value is 0, the person cant donate, but
+        //it will require some gas for the person even if the transaction doesnt go through.
+        //So, I need a way to reject the 0 donation without executing the function fail
         require(msg.value > 0, "Your donate a value");
         p.balance += msg.value;
         return true;
@@ -61,6 +71,8 @@ contract Donation {
 
     function withdraw(uint256 _projectId) public payable returns(bool){
         Project storage p = projects[_projectId];
+        //Same as in the donate() function. Need somehow to abort function execution before
+        //knowing that the msg.sender is not the representant
         require(p.representant == msg.sender, "You can't do this operation");
         p.representant.transfer(p.balance);
         return true;
@@ -72,6 +84,7 @@ contract Donation {
         returns (bool)
     {
         Project storage p = projects[_projectId];
+        //Same as in donate(). I think I can use modifier here, but I'll need to handle its creation
         require(msg.sender == p.representant, "You're not allowed to do this!");
         p.representant = _newRepresentant;
         return true;
